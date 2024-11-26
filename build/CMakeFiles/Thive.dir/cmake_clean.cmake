b66d6d867da893b76bbfb3141af4f391d3d32fe0
elouannh <elouannhosta27@gmail.com>
2024-11-26 22:07:20 +0100

    Pushing work

---
+++ build/CMakeFiles/Thive.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Thive.dir/src/main.cpp.o"
+  "CMakeFiles/Thive.dir/src/main.cpp.o.d"
+  "Thive"
+  "Thive.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Thive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/parser.h
@@ -0,0 +1,6 @@
+#ifndef THIVE_PARSER_H
+#define THIVE_PARSER_H
+
+class Parser;
+
+#endif
